1f698ba2a1dc70cce1f9aed127e7f1785c31dd19
Kathleen Weisensel <kweisensel@gmail.com>
2017-05-09 15:45:41 -0500

    first attempt

---
+++ javascripts/main.js
@@ -0,0 +1,41 @@
+console.log("working main.js");
+
+// Create a simple user interface for your product catalog where a user can select a category
+// from a dropdown.
+// When a category is selected, you must use Promises to read, first, from the categories.json
+// to load that array of objects,
+// then load types.json, then products.json.
+
+// Once all data is loaded, you need to display the products in a Bootstrap grid.
+// Each product must display the string name of its product type, and product category.
+// Not the integer id value.
+
+var output = document.getElementById("output");
+
+var Acme = function(originalAcme){
+	let acmeItems = [];
+	let parseData = function(data){
+		data.categories.forEach( (element) => {
+			acmeItems.push(element);
+		})
+	}
+	originalAcme.loadCategories = () => {
+		return new Promise( (resolve, reject) => {
+			let request = new XMLHttpRequest();
+			request.onload = function(){
+				if (request.status === 200){
+					var data = JSON.parse(request.responseText);
+					parseData(data);
+					resolve(acmeItems);
+				} else {
+					//something went wrong
+					reject(new Error("XMLHttpRequest Error: ", request.statusText));
+				}
+			};
+			request.open("GET", "categories.json");
+			request.send();
+		})
+	}
+	return originalAcme;
+
+}(Acme || {});
